ae4770049119bd3626907d76398be5cc99a8c0e8
jjenus <biz.jenus@gmail.com>
2022-05-23 00:25:56 +0100

    first commit point

---
+++ src/main/java/com/jjenus/securityAppDemo/SecurityAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jjenus.securityAppDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAppDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jjenus/securityAppDemo/SecurityAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjenus.securityAppDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
